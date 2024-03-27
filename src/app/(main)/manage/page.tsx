0bce87a4500ac4f5f965d4ab0567c7191c1e5f45
Đỗ Ngọc Huy <huytrue02@gmail.com>
2024-03-27 10:55:56 +0700

    done marketplace

--- src/app/(main)/manage/page.tsx
+++ src/app/(main)/manage/page.tsx
@@ -47,25 +47,27 @@ export default function Manage() {
       <Card>
         <CardHeader className="items-center justify-center uppercase font-bold text-xl gap-x-1">
           Inventory
-          <Tooltip
-            color="primary"
-            content={"Create new Product"}
-            className="capitalize">
-            <svg
-              onClick={() => router.push("/manage/createProduct")}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-          </Tooltip>
+          {canCreate && (
+            <Tooltip
+              color="primary"
+              content={"Create new Product"}
+              className="capitalize">
+              <svg
+                onClick={() => router.push("/manage/createProduct")}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </Tooltip>
+          )}
         </CardHeader>
         <CardBody>
           <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
