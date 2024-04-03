de85096babec14b10e7ffeecbc08383590cebcef
Đỗ Ngọc Huy <99071294+hyudoo@users.noreply.github.com>
2024-04-03 23:01:41 +0700

    1

--- src/app/(main)/exchange/page.tsx
+++ src/app/(main)/exchange/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import React from "react";
-import { Card, CardBody, CardHeader, Tooltip } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Chip,
+  Tab,
+  Tabs,
+  Tooltip,
+} from "@nextui-org/react";
 import ExchangeCard from "@/components/exchange/ExchangeCard";
 import { IExchange } from "@/_types_";
 import { useAppSelector } from "@/reduxs/hooks";
@@ -9,6 +17,7 @@ import ExchangeProductContract from "@/contracts/ExchangeProductContract";
 import { useRouter } from "next/navigation";
 
 export default function Exchange() {
+  const [selected, setSelected] = React.useState("exchange");
   const { wallet, signer } = useAppSelector((state) => state.account);
   const [isRender, setIsRender] = React.useState<boolean>(true);
   const [exchanges, setExchanges] = React.useState<IExchange[]>([]);
@@ -43,67 +52,130 @@ export default function Exchange() {
   }, [getListExchange]);
 
   return (
-    <div className="flex w-full flex-col gap-y-2">
-      <Card>
-        <CardHeader className="items-center justify-center uppercase font-bold text-xl">
-          Exchange
-          <Tooltip
-            color="primary"
-            content={"Create new Exchange"}
-            className="capitalize">
-            <svg
-              onClick={() => router.push("/exchange/create")}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-          </Tooltip>
-        </CardHeader>
-        <CardBody>
-          <div className="gap-2">
-            {exchanges?.map((exchange, index) => (
-              <ExchangeCard
-                key={index}
-                address={exchange.sender}
-                exchangeId={exchange.id}
-                yourTokenIds={exchange.senderTokenIds}
-                otherTokenIds={exchange.receiverTokenIds}
-                type="exchange"
-                render={() => setIsRender(true)}
-              />
-            ))}
-          </div>
-        </CardBody>
-      </Card>
+    <div className="flex w-full flex-col">
+      <Tabs
+        aria-label="Options"
+        selectedKey={selected}
+        onSelectionChange={(key) => setSelected(key as string)}
+        color="primary"
+        variant="underlined"
+        classNames={{
+          tabList:
+            "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+          cursor: "w-full bg-cyan-600",
+          tab: "max-w-fit px-0 h-12",
+          tabContent: "group-data-[selected=true]:text-cyan-600",
+        }}>
+        <Tab
+          key="inventory"
+          title={
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+                />
+              </svg>
 
-      <Card>
-        <CardHeader className="items-center justify-center uppercase font-bold text-xl">
-          Incoming Exchange
-        </CardHeader>
-        <CardBody>
-          <div className="gap-2">
-            {incomingExchanges?.map((exchange, index) => (
-              <ExchangeCard
-                key={index}
-                address={exchange.sender}
-                exchangeId={exchange.id}
-                yourTokenIds={exchange.receiverTokenIds}
-                otherTokenIds={exchange.senderTokenIds}
-                type="incoming-exchange"
-                render={() => setIsRender(true)}
-              />
-            ))}
-          </div>
-        </CardBody>
-      </Card>
+              <span>Exchange</span>
+              <Chip size="sm" variant="faded">
+                {exchanges?.length}
+              </Chip>
+            </div>
+          }>
+          <Card>
+            <CardHeader className="items-center justify-center uppercase font-bold text-xl">
+              Exchange
+              <Tooltip
+                color="primary"
+                content={"Create new Exchange"}
+                className="capitalize">
+                <svg
+                  onClick={() => router.push("/exchange/create")}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </Tooltip>
+            </CardHeader>
+            <CardBody>
+              <div className="gap-2">
+                {exchanges?.map((exchange, index) => (
+                  <ExchangeCard
+                    key={index}
+                    address={exchange.sender}
+                    exchangeId={exchange.id}
+                    yourTokenIds={exchange.senderTokenIds}
+                    otherTokenIds={exchange.receiverTokenIds}
+                    type="exchange"
+                    render={() => setIsRender(true)}
+                  />
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab
+          key="incoming"
+          title={
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25"
+                />
+              </svg>
+
+              <span>Incoming Exchange</span>
+              <Chip size="sm" variant="faded">
+                {incomingExchanges?.length}
+              </Chip>
+            </div>
+          }>
+          <Card>
+            <CardHeader className="items-center justify-center uppercase font-bold text-xl">
+              Incoming Exchange
+            </CardHeader>
+            <CardBody>
+              <div className="gap-2">
+                {incomingExchanges?.map((exchange, index) => (
+                  <ExchangeCard
+                    key={index}
+                    address={exchange.sender}
+                    exchangeId={exchange.id}
+                    yourTokenIds={exchange.receiverTokenIds}
+                    otherTokenIds={exchange.senderTokenIds}
+                    type="incoming-exchange"
+                    render={() => setIsRender(true)}
+                  />
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
--- src/app/(main)/inventory/page.tsx
+++ src/app/(main)/inventory/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import React from "react";
-import { Card, CardBody, CardHeader, Tooltip } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Chip,
+  Tab,
+  Tabs,
+  Tooltip,
+} from "@nextui-org/react";
 import ProductCard from "@/components/product/ProductCard";
 import { IProductItem } from "@/_types_";
 import SupplyChainContract from "@/contracts/SupplyChainContract";
@@ -10,6 +18,7 @@ import MarketContract from "@/contracts/MarketPlaceContract";
 import { useRouter } from "next/navigation";
 
 export default function Manage() {
+  const [selected, setSelected] = React.useState("inventory");
   const { wallet, signer } = useAppSelector((state) => state.account);
   const router = useRouter();
   const [listedproducts, setListedProducts] = React.useState<IProductItem[]>();
@@ -45,17 +54,25 @@ export default function Manage() {
   }, [getListProduct]);
 
   return (
-    <div className="flex w-full flex-col gap-y-2">
-      <Card>
-        <CardHeader className="items-center justify-center uppercase font-bold text-xl gap-x-1">
-          Inventory
-          {canCreate && (
-            <Tooltip
-              color="primary"
-              content={"Create new Product"}
-              className="capitalize">
+    <div className="flex w-full flex-col">
+      <Tabs
+        aria-label="Options"
+        selectedKey={selected}
+        onSelectionChange={(key) => setSelected(key as string)}
+        color="primary"
+        variant="underlined"
+        classNames={{
+          tabList:
+            "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+          cursor: "w-full bg-cyan-600",
+          tab: "max-w-fit px-0 h-12",
+          tabContent: "group-data-[selected=true]:text-cyan-600",
+        }}>
+        <Tab
+          key="inventory"
+          title={
+            <div className="flex items-center space-x-2">
               <svg
-                onClick={() => router.push("/manage/createProduct")}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -65,49 +82,102 @@ export default function Manage() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
                 />
               </svg>
-            </Tooltip>
-          )}
-        </CardHeader>
-        <CardBody>
-          <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-            {inventory?.map((product, index) => (
-              <ProductCard
-                key={index}
-                productId={product.id}
-                name={product.name}
-                image={product.images[0]}
-                price={product.price}
-                render={() => setIsRender(true)}
-                type="inventory"
-              />
-            ))}
-          </div>
-        </CardBody>
-      </Card>
-
-      <Card>
-        <CardHeader className="items-center justify-center uppercase font-bold text-xl">
-          Your Listed Products
-        </CardHeader>
-        <CardBody>
-          <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-            {listedproducts?.map((product, index) => (
-              <ProductCard
-                key={index}
-                name={product.name}
-                image={product.images[0]}
-                price={product.price}
-                productId={product.id}
-                type="unlist"
-                render={() => setIsRender(true)}
-              />
-            ))}
-          </div>
-        </CardBody>
-      </Card>
+              <span>Inventory</span>
+              <Chip size="sm" variant="faded">
+                {inventory?.length}
+              </Chip>
+            </div>
+          }>
+          <Card>
+            <CardHeader className="items-center justify-center uppercase font-bold text-xl gap-x-1">
+              Inventory
+              {canCreate && (
+                <Tooltip
+                  color="primary"
+                  content={"Create new Product"}
+                  className="capitalize">
+                  <svg
+                    onClick={() => router.push("/manage/createProduct")}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </Tooltip>
+              )}
+            </CardHeader>
+            <CardBody>
+              <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {inventory?.map((product, index) => (
+                  <ProductCard
+                    key={index}
+                    productId={product.id}
+                    name={product.name}
+                    image={product.images[0]}
+                    price={product.price}
+                    render={() => setIsRender(true)}
+                    type="inventory"
+                  />
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab
+          key="listed"
+          title={
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+              <span>Listed</span>
+              <Chip size="sm" variant="faded">
+                {listedproducts?.length}
+              </Chip>
+            </div>
+          }>
+          <Card>
+            <CardHeader className="items-center justify-center uppercase font-bold text-xl">
+              Your Listed Products
+            </CardHeader>
+            <CardBody>
+              <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {listedproducts?.map((product, index) => (
+                  <ProductCard
+                    key={index}
+                    name={product.name}
+                    image={product.images[0]}
+                    price={product.price}
+                    productId={product.id}
+                    type="unlist"
+                    render={() => setIsRender(true)}
+                  />
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
